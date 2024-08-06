c07a276181ded81a007150d7b27c87af1b981006
denismnjima <denismnjima@gmail.com>
2024-08-06 22:47:05 +0300

    moved components directory from assets to src

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/components/App.jsx'
+import App from './components/App.jsx'
 import './index.css'
 import "semantic-ui-css/semantic.min.css";
 
